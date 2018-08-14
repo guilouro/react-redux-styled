af6730c3801c0351ff38098b991a3f90c90d052d
Guilherme Louro <guilherme-louro@hotmail.com>
2018-08-14 11:23:17 -0300

    Added Favicon component

--- src/root.js
+++ src/root.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
+import Favicon from 'react-favicon';
 import configureStore from './configureStore';
 import Routes from './Routes';
+import ico from '../images/react.png';
 
 export default () => (
     <Provider store={configureStore()}>
-        <Routes />
+        <Fragment>
+            <Favicon url={ico} />
+            <Routes />
+        </Fragment>
     </Provider>
 );
