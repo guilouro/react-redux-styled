458d3a4aac969ff1eee56dc677ba80e9e255cebe
Guilherme Louro <guilherme-louro@hotmail.com>
2018-08-14 11:00:34 -0300

    buil command

--- webpack/dev.js
+++ webpack/dev.js
@@ -5,25 +5,15 @@ export default {
     ...baseConfig,
     mode: 'development',
 
-    // entry: [
-    //     'react-hot-loader/patch',
-    //     'webpack-dev-server/client?http://localhost:8000/#/',
-    //     ...baseConfig.entry
-    // ],
-
     devtool: '#inline-source-map',
 
     plugins: [
         ...baseConfig.plugins,
         new webpack.HotModuleReplacementPlugin(),
-        // new DashboardPlugin(dashboard.setData),
     ],
 
     devServer: {
-        // quiet: true,
         hot: true,
         port: '8000',
-        // inline: true,
-        // historyApiFallback: true,
-    }
-}
+    },
+};
